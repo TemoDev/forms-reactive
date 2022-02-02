8a3d37240a2d0fa6df9a4d8afb1dc422ea9bcdb6
TemoDev <temo.kuliashvili@gmail.com>
2022-02-02 08:09:43 -0800

    Finished Project Added Custom Validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,17 +9,27 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class AppComponent implements OnInit {
 
   assignmentForm: FormGroup;
+  forbiddenName = ['Test'];
 
   ngOnInit(): void {
       this.assignmentForm = new FormGroup ({
-        'prName': new FormControl(null, Validators.required),
+        'prName': new FormControl(null, [Validators.required, this.noName.bind(this)]),
         'email': new FormControl(null, [Validators.required, Validators.email]),
         'prStatus': new FormControl('stable')
       })
   }
 
   submitForm(){
-    console.log(this.assignmentForm);
+    console.log('Project name is:', this.assignmentForm.value.prName);
+    console.log('Your email is:', this.assignmentForm.value.email);
+    console.log('Project status is:', this.assignmentForm.value.prStatus);
+  }
+
+  noName(control: FormControl): {[s:string]:boolean} {
+    if(this.forbiddenName.indexOf(control.value) !== -1){
+      return {'nameIsForbidden': true};
+    }
+    return null;
   }
 
 }
